6159ac67306d05cd8a557475b483b29fd59bae34
Andrea P <90342001+MugugnoVero@users.noreply.github.com>
2021-12-05 17:12:18 +0100

    Add files via upload
    R grapics graded assignment. Plot 1-4 with R code to construct each plot.

---
+++ Plot1.R
@@ -0,0 +1,52 @@
+#-----------------------------------------#
+#         Peer graded assignment.         #
+#-----------------------------------------#
+
+#### Load libraries and prepare session. ####
+
+library(RMugugno)
+
+link = "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+
+handle_remote_file = function(x, setDT = TRUE){
+  destdir = tempdir()
+  dfile = tempfile(tmpdir = destdir, fileext = ".zip")
+  download.file(x, destfile = dfile)
+  unzip(dfile, exdir = destdir)
+  file = list.files(destdir, full.names = T, pattern = ".txt")
+  res = read.csv2(file, na.strings = "?")
+  if(setDT){
+    setDT(res)
+  }
+  return(res)
+}
+
+# f = cache_function(handle_remote_file, 'second_assignment', force = T, folder = conf$default_temp_folder)
+f = cache_function(handle_remote_file, 'second_assignment', force = F, folder = conf$default_temp_folder)
+res = f(link)
+
+# Convert.
+res[, `:=` (
+  Date = as.Date(Date, format = "%d/%m/%Y"),
+  Time = strptime(paste(as.Date(Date, format = "%d/%m/%Y"), Time, sep = " "), format = "%Y-%m-%d %H:%M:%S")
+  )]
+
+# Subset.
+res = res[Date >= as.Date("2007-02-01") & Date <= as.Date("2007-02-02"), list(
+  Date,
+  Time,
+  Global_active_power = as.numeric(Global_active_power),
+  Global_reactive_power = as.numeric(Global_reactive_power),
+  Voltage = as.numeric(Voltage),
+  Global_intensity = as.numeric(Global_intensity),
+  Sub_metering_1 = as.numeric(Sub_metering_1),
+  Sub_metering_2 = as.numeric(Sub_metering_2),
+  Sub_metering_3 = as.numeric(Sub_metering_3)
+)]
+
+# Plot 1
+with(data = res, {
+  png(filename = "C:/Users/Andrea/Desktop/Plot1.png")
+  hist(Global_active_power, col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)")
+  dev.off()
+})
---
+++ Plot3.R
@@ -0,0 +1,56 @@
+#-----------------------------------------#
+#         Peer graded assignment.         #
+#-----------------------------------------#
+
+#### Load libraries and prepare session. ####
+
+library(RMugugno)
+
+link = "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+
+handle_remote_file = function(x, setDT = TRUE){
+  destdir = tempdir()
+  dfile = tempfile(tmpdir = destdir, fileext = ".zip")
+  download.file(x, destfile = dfile)
+  unzip(dfile, exdir = destdir)
+  file = list.files(destdir, full.names = T, pattern = ".txt")
+  res = read.csv2(file, na.strings = "?")
+  if(setDT){
+    setDT(res)
+  }
+  return(res)
+}
+
+# f = cache_function(handle_remote_file, 'second_assignment', force = T, folder = conf$default_temp_folder)
+f = cache_function(handle_remote_file, 'second_assignment', force = F, folder = conf$default_temp_folder)
+res = f(link)
+
+# Convert.
+res[, `:=` (
+  Date = as.Date(Date, format = "%d/%m/%Y"),
+  Time = strptime(paste(as.Date(Date, format = "%d/%m/%Y"), Time, sep = " "), format = "%Y-%m-%d %H:%M:%S")
+)]
+
+# Subset.
+res = res[Date >= as.Date("2007-02-01") & Date <= as.Date("2007-02-02"), list(
+  Date,
+  Time,
+  Global_active_power = as.numeric(Global_active_power),
+  Global_reactive_power = as.numeric(Global_reactive_power),
+  Voltage = as.numeric(Voltage),
+  Global_intensity = as.numeric(Global_intensity),
+  Sub_metering_1 = as.numeric(Sub_metering_1),
+  Sub_metering_2 = as.numeric(Sub_metering_2),
+  Sub_metering_3 = as.numeric(Sub_metering_3)
+)]
+
+# Plot 3
+with(data = res, {
+  dev.new()
+  png(filename = "C:/Users/Andrea/Desktop/Plot3.png")
+  plot(Time, Sub_metering_1, type = "l", ylab = "Energy sub meetering")
+  lines(Time, Sub_metering_2, type = "l", col = "red")
+  lines(Time, Sub_metering_3, type = "l", col = "blue")
+  legend("topright", lwd = 1, cex = 1, col = c("black", "red", "blue"), legend = paste("Sub_meetering_", 1:3))
+  dev.off()
+})
